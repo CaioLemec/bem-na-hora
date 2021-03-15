5c600fdbb9a833278b5e95cac66567b83b14419c
Caio Lemec <caiolemec@gmail.com>
2021-03-15 11:36:10 -0300

    Adding new functions

--- scripts/map.js
+++ scripts/map.js
@@ -1,33 +1,47 @@
-const mymap = L.map("mapid").setView([-23.5879824,-46.6616683], 14);
+//Habilitando mapa. Tutorial em https://leafletjs.com/
+const mymap = L.map("mapid").setView([-23.5879824, -46.6616683], 14);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {minZoom:15}).addTo(mymap);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(mymap);
+//Criando constantes para facilitar código.
+const base_url = "https://aiko-olhovivo-proxy.aikodigital.io";
+const token = "35779ce7c804e10322855a99110a9c1bf818aac328c3d591edb969f7972c4460";
 
-// const fetch = require('node-fetch')
-
-const base_url = 'https://aiko-olhovivo-proxy.aikodigital.io'
-const token = '35779ce7c804e10322855a99110a9c1bf818aac328c3d591edb969f7972c4460'
-
+// Criando parametros
 const headers = {
-  'Accept': 'application/json',
-  'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': 'https://aiko-olhovivo-proxy.aikodigital.io/',
+  Accept: "application/json",
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "https://aiko-olhovivo-proxy.aikodigital.io/",
 };
 
-//Criando autenticação
+//Função de autenticação - Seguindo orientação da API OlhoVivo.
 async function auth() {
-  const response = await (await fetch(`${base_url}/login/autenticar?token=${token}`, { method: "POST", headers })).json()
-  console.log(response)
+  const response = await (
+    await fetch(`${base_url}/login/autenticar?token=${token}`, {
+      method: "POST",
+      headers,
+    })
+  ).json();
+  console.log(response);
 }
 
-//Buscando uma linhas a partir de um input
+//Autenticando onload.
+window.onload = auth();
+
+
+//Buscando uma linha a partir de um input
 async function getLines() {
   const inputClient = document.querySelector("#lines").value;
-  const lines = await (await fetch(`${base_url}/Linha/Buscar?termosBusca=${inputClient}`, { method: 'GET', headers })).json()
-  lines.forEach(linhas => {
-    if (linhas.sl == 1) {
-        console.log(`Código da linha = ${linhas.cl}, Sentido ${linhas.tp}`)
+  const lines = await (
+    await fetch(`${base_url}/Linha/Buscar?termosBusca=${inputClient}`, {
+      method: "GET",
+      headers,
+    })
+  ).json();
+  lines.forEach((lines) => {
+    if (lines.sl === 1) {
+      console.log(`Código da linha = ${lines.cl}, Sentido ${lines.tp}`);
     } else {
-        console.log(`Código da linha = ${linhas.cl}, Sentido ${linhas.ts}`)
+      console.log(`Código da linha = ${lines.cl}, Sentido ${lines.ts}`);
     }
   });
 }
@@ -35,22 +49,64 @@ async function getLines() {
 //Buscando uma parada a partir de um input
 async function getStops() {
   const inputClient = document.querySelector("#stops").value;
-  const stops = await (await fetch(`${base_url}/Parada/Buscar?termosBusca=${inputClient}`, { method: 'GET', headers })).json()
-  stops.forEach(stops => {
-
-  var marker = L.marker([stops.py, stops.px]).addTo(mymap);
-  marker.bindPopup(`<strong>PARADA:</strong> ${stops.np}<br><strong>ENDEREÇO:</strong> ${stops.ed}`).openPopup();
+  const stops = await (
+    await fetch(`${base_url}/Parada/Buscar?termosBusca=${inputClient}`, {
+      method: "GET",
+      headers,
+    })
+  ).json();
+  stops.forEach((stops) => {
+    var marker = L.marker([stops.py, stops.px]).addTo(mymap);
+    marker
+      .bindPopup(
+        `
+          <strong>CÓDIGO: </strong>${stops.cp} <br>
+          <strong>NOME: </strong>${stops.np} <br>
+          <strong>ENDEREÇO: </strong>${stops.ed}
+        `
+      )
+      .openPopup();
   });
 }
 
 // Posições dos veículos: Exibir no mapa onde os veículos estavam na sua última atualização.
 async function getVehicles() {
-  const vehicles = await (await fetch(`${base_url}/Posicao`, { method: 'GET', headers })).json()
-  Object.keys(vehicles).forEach((key) => {
-    console.log(vehicles[key]); 
-    // percorrer o array e coletar cada px,py para imprimir no mapa.
+  const response = await (
+    await fetch(`${base_url}/Posicao`, { method: "GET", headers })
+  ).json();
+  const vehicles = response.l;
+  vehicles.forEach(({ vs, cl, lt0 }) => {
+    const [coordinates] = vs;
+    var myIcon = L.icon({
+      iconUrl: '../images/bus.png',
+      iconSize: [45, 45]
+  });
+    var marker = L.marker([coordinates.py, coordinates.px], {icon: myIcon}).addTo(mymap);
+    marker
+      .bindPopup(
+        `<strong>Código Linha:</strong> ${cl}<br><strong>Destino:</strong> ${lt0}`
+      )
+      .openPopup();
   });
 }
+
+// Previsão de chegada: Dado uma parada informar a previsão de chegada de cada veículo que passe pela parada selecionada.
+
+async function arrivalForecast() {
+  const arrivalsStop = document.querySelector("#arrivals-stop").value;
+  const arrivalsLine = document.querySelector("#arrivals-line").value;
+  const response = await (
+    await fetch(`${base_url}/Previsao?codigoParada=${arrivalsStop}&codigoLinha=${arrivalsLine}`, {
+      method: "GET",
+      headers,
+    })
+  ).json();
+  const getLineInfo = response.l;
+  const getStopInfo = response.p;
+  console.log(getLineInfo, getStopInfo, response)
+}
+
+
 
 
 
@@ -58,8 +114,6 @@ async function getVehicles() {
 
 // Desafio: https://github.com/aikodigital/programa-estagio-2021/blob/main/front-end.md
 
-// Requesitos: (FALTA)
 
-// Previsão de chegada: Dado uma parada informar a previsão de chegada de cada veículo que passe pela parada selecionada.
-
-// Pesquisa e Filtros: Permitir que o usuário pesquise e filtre esses dados, interagindo com a interface.
+// 1337 CL
+// 440015007 CP
